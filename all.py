eac26bea81af8ab4b3e3bc9802e0dbf44fa9aaa8
Zekang <zkcheng721@gmail.com>
2019-04-11 21:32:09 +0100

    today

--- all.py
+++ all.py
@@ -514,6 +514,18 @@ class Solution(object):
         helper([])
         return res
 
+# 48. Rotate Image
+# 70%
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n//2 + 1):
+            for j in range(i, n - 1 - i):
+                matrix[i][j], matrix[j][n - 1 - i], matrix[n - 1 - i][n - 1 - j], matrix[n - j - 1][i] = \
+                matrix[n - j - 1][i], matrix[i][j], matrix[j][n - 1 - i], matrix[n - 1 - i][n - 1 - j]
+
 # 51. N-Queens
 # 58%
     def solveNQueens(self, n: int) -> List[List[str]]:
